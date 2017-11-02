723497091b09f027cd4f3b5a91a54cc19c00c44c
Scott Pidzarko <scottpidzarko@users.noreply.github.com>
2017-11-01 18:48:26 -0700

    Add in code that got deleted by accident pre-commit

--- BeerMachineCode/BeerMachineCode.ino
+++ BeerMachineCode/BeerMachineCode.ino
@@ -7,6 +7,26 @@
 
 
 
+const int chipSelect = 53;
+const int name_offset = 11;
+const int beer_offset = 22;
+const int lifetime_offset = 26;
+const int password_offset = 4;
+const int sd_power = 48;
+
+//********Keypad Setup********
+#include <Keypad.h>
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {
+  3, 8, 7, 5}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {
   4, 2, 6,9}; //connect to the column pinouts of the keypad
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 char key;
